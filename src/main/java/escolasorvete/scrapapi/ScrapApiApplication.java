46137d9edb4be5ff17cd41760de00708bc009b04
nicolas-escolasorvete <nicolas@escolasorvete.com.br>
2023-02-24 07:57:53 -0300

    initial

---
+++ src/main/java/escolasorvete/scrapapi/ScrapApiApplication.java
@@ -0,0 +1,13 @@
+package escolasorvete.scrapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/escolasorvete/scrapapi/ScrapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package escolasorvete.scrapapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
